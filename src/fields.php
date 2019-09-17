bcf1c3d25e2657bc0023068be637c39e28c3488e
Christian Widlund <christian.widlund@isotop.se>
2019-09-17 09:49:48 +0200

    Add recaptcha 3 field

--- src/fields.php
+++ src/fields.php
@@ -17,3 +17,16 @@ forms()->add_field( 'recaptcha', function ( $attributes ) {
 
 	return sprintf( '<div class="g-recaptcha" %s></div>', forms()->html_attributes( $attributes['recaptcha'], 'data-' ) );
 } );
+
+/**
+ * Add reCAPTCHA3 field.
+ */
+forms()->add_field( 'recaptcha3', function ( $attributes ) {
+	$attributes = array_merge( [
+		'recaptcha3' => []
+	], $attributes );
+
+	return sprintf( '<input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response" %s>', forms()
+		->html_attributes(
+			$attributes['recaptcha3'], 'data-' ) );
+} );
